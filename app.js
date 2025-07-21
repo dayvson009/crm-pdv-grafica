9d5d9c917689b99820926a3465bdcb95ad9ce474
dayvson009 <dayvson.vinicius@hotmail.com>
2025-07-21 01:30:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -19,25 +19,3 @@ app.use('/', routes);
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
-=======
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const routes = require('./routes');
-
-const app = express();
-const PORT = 3000;
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
->>>>>>> 52ed5e0d40442642bf2e145afd1b21befd53940c
